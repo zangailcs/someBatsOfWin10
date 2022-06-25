70b90009dbcc2c6d4b854f9ac810813b3cc8b46c
lichangshun <1183706328@qq.com>
2022-06-25 21:34:43 +0800

    first commit

---
+++ reopenAfterRestart.bat
@@ -0,0 +1,13 @@
+echo "start typora"
+start /d "D:\softs\workSoft\Typora" Typora.exe
+
+echo "start IDEA"
+start /d "D:\softs\ide\IDEA2022.1.2\bin" idea64.exe
+
+echo "start sqlyog"
+start /d "D:\softs\ide\SQLYog" SQLyogCommunity.exe
+
+echo "open git bash"
+start "" "D:\softs\ide\Git\git-bash.exe" -c "cd D:/codes/java/MybatisStudy && bash"
+
+start "" "D:\softs\ide\Git\git-bash.exe" -c "cd D:/codes/blogs/lics_blogs && bash"
