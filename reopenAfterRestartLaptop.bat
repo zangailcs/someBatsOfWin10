3273679592951911c51669b9e2e99680106da5ef
lichangshun <1183706328@qq.com>
2022-06-26 20:41:29 +0800

    add laptop

---
+++ reopenAfterRestartLaptop.bat
@@ -0,0 +1,13 @@
+echo "start typora"
+start /d "D:\Softs\Typora" Typora.exe
+
+echo "start IDEA"
+start /d "D:\Softs\idea\IntelliJIDEA2021.2\bin" idea64.exe
+
+echo "start sqlyog"
+start /d "D:\Softs\SQLyogCommunity" SQLyogCommunity.exe
+
+echo "open git bash"
+start "" "C:\Program Files\Git\git-bash.exe" -c "cd D:/codes/22learning/MybatisStudy && bash"
+
+start "" "C:\Program Files\Git\git-bash.exe" -c "cd D:/codes/22learning/lics_blogs && bash"
